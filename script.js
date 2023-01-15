47e75c5a9fb9bdedf0689c51dec807c20feede0b
Alumetriz <fanmax0808@gmail.com>
2023-01-15 21:26:04 +0200

    feat: add input logic

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 'use strict';
+/*
+console.log(document.querySelector('.guess-message').textContent);
+
+document.querySelector('.guess-message').textContent = 'Правильно!';
+
+document.querySelector('.question').textContent = 7;
+
+document.querySelector('.score').textContent = 11;
+
+document.querySelector('.number-input').value
+*/
+
+// const eventHandler = function() {
+//   console.log(document.querySelector('.number-input').value);
+// }
+document.querySelector('.check')
+  .addEventListener('click', function() {
+    const guessingNumber =
+      Number(document.querySelector('.number-input').value);
+    console.log(guessingNumber, typeof guessingNumber);
+
+    if (!guessingNumber) {
+      document.querySelector('.guess-message')
+        .textContent = 'Введите какое-то число!';
+    }
+  });
+
