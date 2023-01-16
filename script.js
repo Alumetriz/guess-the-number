0a3af37d71bef3ad8a524a1b235e31b2e0673afe
Alumetriz <fanmax0808@gmail.com>
2023-01-16 16:13:35 +0200

    feat: implemented functionality

--- script.js
+++ script.js
@@ -3,11 +3,10 @@
 //   console.log(document.querySelector('.number-input').value);
 // }
 
-const secretNumber =
-  Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
-
+let highScore = 0;
 /*
 В этом случае: Math.trunc(Math.random() * 20)
 Я буду получать значение между 0 и 19, т.к. все десятичные знаки будут откинуты.
@@ -15,12 +14,6 @@ let score = 20;
 Но так как мне нужно значение между 1 и 20 - то я прибавляю 1:
 Math.trunc(Math.random() * 20) + 1
  */
-// del
-
-// del
-
-
-console.log(secretNumber);
 document.querySelector('.check')
   .addEventListener('click', function() {
     const guessingNumber =
@@ -43,7 +36,11 @@ document.querySelector('.check')
       document.querySelector('.question')
         .style.width = '50rem'
 
-
+      if (score > highScore) {
+        highScore = score;
+      }
+      document.querySelector('.highscore')
+        .textContent = highScore;
       // Too high
     } else if (guessingNumber > secretNumber) {
       if (score > 1) {
@@ -72,7 +69,26 @@ document.querySelector('.check')
           .textContent = 'Game Over!';
         document.querySelector('.score')
           .textContent = 0;
+        document.querySelector('body')
+          .style.backgroundColor = 'rgb(255,0,0)'
       }
     }
   });
 
+document.querySelector('.again')
+  .addEventListener('click', function(){
+    // new secret number
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    score = 20;
+    // del
+    console.log(secretNumber);
+    // del
+    document.querySelector('.guess-message')
+      .textContent = 'Начни угадывать!';
+    document.querySelector('.number-input').value = '';
+    document.querySelector('body').style.backgroundColor = 'rgb(0, 0, 0)'
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.question').textContent = '???'
+    document.querySelector('.question').style.width = '25rem';
+  })
+console.log(secretNumber);
