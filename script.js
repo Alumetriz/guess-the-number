1df7d5f33ace2a93317be9ffdeafd15235601588
Alumetriz <fanmax0808@gmail.com>
2023-01-16 16:57:17 +0200

    refactor: Code refactoring

--- script.js
+++ script.js
@@ -7,8 +7,14 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 Но так как мне нужно значение между 1 и 20 - то я прибавляю 1:
 Math.trunc(Math.random() * 20) + 1
  */
+
 let score = 20;
 let highScore = 0;
+
+const displayMessage = function(selector, message) {
+  document.querySelector(selector).textContent = message;
+}
+
 document.querySelector('.check')
   .addEventListener('click', function() {
     const guessingNumber =
@@ -17,69 +23,54 @@ document.querySelector('.check')
 
     // No input
     if (!guessingNumber) {
-      document.querySelector('.guess-message')
-        .textContent = 'Введите число!';
+      displayMessage('.guess-message', 'Введите число!')
 
       // Player won
     } else if (guessingNumber === secretNumber) {
       document.querySelector('.guess-message')
         .textContent = 'Правильно!';
+      displayMessage('.guess-message', 'Правильно!')
       document.querySelector('.question')
         .textContent = secretNumber;
       document.querySelector('body')
         .style.backgroundColor = 'rgb(9, 250, 21)';
       document.querySelector('.question')
-        .style.width = '50rem'
+        .style.width = '50rem';
 
       if (score > highScore) {
         highScore = score;
       }
       document.querySelector('.highscore')
         .textContent = highScore;
-      // Too high
-    } else if (guessingNumber > secretNumber) {
-      if (score > 1) {
-        document.querySelector('.guess-message')
-          .textContent = 'Слишком много!';
-        score--;
-        document.querySelector('.score')
-          .textContent = score;
-      } else {
-        document.querySelector('.guess-message')
-          .textContent = 'Game Over!';
-        document.querySelector('.score')
-          .textContent = 0;
-      }
 
-      // Too low
-    } else if (guessingNumber < secretNumber) {
+      // Number from input is wrong
+    } else if (guessingNumber !== secretNumber) {
       if (score > 1) {
-        document.querySelector('.guess-message')
-          .textContent = 'Слишком мало!';
+        displayMessage('.guess-message',
+          guessingNumber > secretNumber ? 'Слишком много!' : 'Слишком мало!'
+        );
         score--;
         document.querySelector('.score')
           .textContent = score;
       } else {
-        document.querySelector('.guess-message')
-          .textContent = 'Game Over!';
+        displayMessage('.guess-message', 'Game Over!')
+        document.querySelector('body')
+          .style.backgroundColor = 'rgb(255, 0, 0)'
         document.querySelector('.score')
           .textContent = 0;
-        document.querySelector('body')
-          .style.backgroundColor = 'rgb(255,0,0)'
       }
     }
   });
 
 document.querySelector('.again')
-  .addEventListener('click', function(){
+  .addEventListener('click', function() {
     // new secret number
     secretNumber = Math.trunc(Math.random() * 20) + 1;
     score = 20;
-    document.querySelector('.guess-message')
-      .textContent = 'Начни угадывать!';
+    displayMessage('.guess-message', 'Начни угадывать!')
     document.querySelector('.number-input').value = '';
-    document.querySelector('body').style.backgroundColor = 'rgb(0, 0, 0)'
+    document.querySelector('body').style.backgroundColor = 'rgb(0, 0, 0)';
     document.querySelector('.score').textContent = score;
-    document.querySelector('.question').textContent = '???'
+    document.querySelector('.question').textContent = '???';
     document.querySelector('.question').style.width = '25rem';
-  })
+  });
